08d37c49085ec27fa83b02c39b4b4a86e9e0bbc4
Aldenhovel Liang <39938763+Aldenhovel@users.noreply.github.com>
2021-04-18 20:41:16 +0800

    Add files via upload

--- nAlgorithm.cpp
+++ nAlgorithm.cpp
@@ -12,13 +12,29 @@ namespace stl::Algorithm{
     }
 
     template <typename T>
-    void Sort::swap(T& a, T& b) {
+    void swap(T& a, T& b) {
         T temp = a;
         a = b;
         b = temp;
     }
 
-    void Sort::bubbleSort(vector<int>& arr, bool ASC=true) {
+    void Sort::reverse(vector<int>& arr){
+        int length = arr.size();
+        int temp;
+        for (int i=0; i<length/2; i++) swap(arr[i], arr[length-i-1]);
+    }
+    void Sort::reverse(vector<float>& arr){
+        int length = arr.size();
+        int temp;
+        for (int i=0; i<length/2; i++) swap(arr[i], arr[length-i-1]);
+    }
+    void Sort::reverse(vector<double>& arr){
+        int length = arr.size();
+        int temp;
+        for (int i=0; i<length/2; i++) swap(arr[i], arr[length-i-1]);
+    }
+
+    void Sort::bubbleSort(vector<int>& arr, bool ASC) {
         int length = arr.size();
         if (ASC){
             for (int i=length-1; i>0; i--){
@@ -35,5 +51,17 @@ namespace stl::Algorithm{
             }
         }
         cout << "BubbleSort Done!" << endl;
+    }
+
+    void Sort::insertSort(vector<int>& arr, bool ASC) {
+        int temp = 0;
+        int i, j;
+        for (i=1; i<arr.size(); i++){
+            temp = arr[i];
+            for (j=i-1; temp<arr[j] && j>=0; j--){
+                arr[j+1] = arr[j];
+            }
+            arr[j+1] = temp;
+        }
     }
 }
--- nAlgorithm.h
+++ nAlgorithm.h
@@ -9,12 +9,21 @@ using namespace std;
 
 namespace stl{
     namespace Algorithm{
+
+        template <typename T>
+        void swap(T&, T&);
+
+
+
         class Sort{
+        private:
         public:
             Sort();
-
-            template <typename T> static void swap(T&, T&);
             static void bubbleSort(vector<int>&, bool);
+            static void reverse(vector<int>&);
+            static void reverse(vector<float>&);
+            static void reverse(vector<double>&);
+            static void insertSort(vector<int>&, bool);
         };
     }
 }
